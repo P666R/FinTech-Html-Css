4f708441183d6a31b0b98119ae6d84bd03cf7357
PRoy <sam469roger@gmail.com>
2024-06-21 08:48:45 +0530

    first commit

---
+++ basic/script.js
@@ -0,0 +1,54 @@
+// header scrolling
+
+window.addEventListener('scroll', function () {
+  const header = document.getElementsByClassName('header-main')[0];
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+
+// hamburger
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const accordionItems = document.querySelectorAll(
+    '.features-progress-content-accordion-item'
+  );
+
+  accordionItems.forEach((item) => {
+    const header = item.querySelector(
+      '.features-progress-content-accordion-item-thumb'
+    );
+    header.addEventListener('click', () => {
+      const isActive = item.classList.contains('is-active');
+      closeAllPanels();
+      if (!isActive) {
+        openPanel(item);
+      }
+    });
+  });
+
+  function closeAllPanels() {
+    accordionItems.forEach((item) => {
+      item.classList.remove('is-active');
+      const icon = item.querySelector('.accordion-icon');
+      icon.src = 'https://img.icons8.com/android/24/228BE6/plus.png';
+    });
+  }
+
+  function openPanel(item) {
+    item.classList.add('is-active');
+    const icon = item.querySelector('.accordion-icon');
+    icon.src = 'https://img.icons8.com/material-outlined/24/228BE6/minus.png';
+  }
+});
